e885b5603bb16d246148b2cbcd1e014fa4ecc959
Jay Khakim <mgmediajay@gmail.com>
2024-05-01 23:36:32 +0900

    FEAT: swagger implemented for main and user routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,21 @@ import {
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiFoundResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("User")
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   
   @Post('register')
+  @ApiCreatedResponse({
+    description: "Created user object as responce",
+    type: User
+  })
+  @ApiBadRequestResponse({
+    description: "User cannot register. Try again"
+  })
   async registerUser(@Body() userData: CreateUserDto): Promise<any> {
     try {
       const res = await this.usersService.registerUser(userData);
@@ -25,6 +34,9 @@ export class UsersController {
     }
   }
 
+  @ApiFoundResponse({
+    description: "User with this emas has been found"
+  })
   @Get('email')
   async findByEmail(@Param('email') email: string): Promise<string> {
     console.log(email)
